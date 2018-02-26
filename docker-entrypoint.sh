bb2a3d0d1dc55bb661e5f918930c53e9a5f36a28
Eddy Hintze <eddy@hintze.co>
2018-02-26 11:14:51 -0500

    encode message queue password to be url safe

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,7 @@ echo $IS_WORKER
 cd /src/
 
 if $IS_WORKER; then
-    echo "Run worker"
     celery -A slackbot worker --loglevel=info
 else
-    echo "Run listener"
     python3 -m slackbot.run_tasks
 fi
--- slackbot/celery.py
+++ slackbot/celery.py
@@ -1,10 +1,12 @@
 import os
 import yaml
+import urllib.parse
 from celery import Celery
 
 
 config = yaml.load(open(os.environ['SB_CONFIG'], 'r'))
-
+# Make the password url safe
+config['RABBITMQ']['PASSWORD'] = urllib.parse.quote(config['RABBITMQ']['PASSWORD'])
 app = Celery('slackbot',
              broker='amqp://{USER}:{PASSWORD}@{HOST}:5672/{VHOST}'.format(**config['RABBITMQ']),
              backend='rpc://',
